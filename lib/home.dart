1e713cd1781e554148323ad8348aced0e641224a
Arkadiusz Żmudzin <arek@moodup.team>
2021-03-21 23:52:40 +0100

    Load comic books from API

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:marvel_comics/api/marvel-api-client.dart';
 import 'package:marvel_comics/comics_list_item.dart';
+
+import 'models/comic_book.dart';
 
 class HomeWidget extends StatefulWidget {
   @override
@@ -7,13 +10,36 @@ class HomeWidget extends StatefulWidget {
 }
 
 class _HomeWidgetState extends State<HomeWidget> {
+  Future<List<ComicBook>> comicBooks;
+
+  @override
+  void initState() {
+    super.initState();
+    comicBooks = MarvelApiClient().getComicBooks();
+  }
+
   @override
   Widget build(BuildContext context) {
+    return FutureBuilder<List<ComicBook>>(
+        future: comicBooks,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return comicBookList(snapshot.data);
+          } else if (snapshot.hasError) {
+            return Center(child: Text("${snapshot.error}"));
+          }
+
+          return Center(child: CircularProgressIndicator());
+        });
+  }
+
+  Widget comicBookList(List<ComicBook> comicBooks) {
     return ListView.builder(
-      padding: EdgeInsets.all(16),
-      itemBuilder: (context, i) {
-        return ComicsListItemWidget("Title $i", "Description $i");
-      }
-    );
+        itemCount: comicBooks.length,
+        padding: EdgeInsets.all(16),
+        itemBuilder: (context, i) {
+          return ComicsListItemWidget(comicBooks.elementAt(i).title,
+              comicBooks.elementAt(i).description);
+        });
   }
 }
