2a51e2f989d1277cd09b30173702e2d2243a91de
Arkadiusz Żmudzin <arek@moodup.team>
2021-03-21 18:00:29 +0100

    Add widget for displaying card with comics

---
+++ lib/comics_list_item.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class ComicsListItemWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Row(
+        children: [
+          Image.asset(
+            'assets/images/cover.jpeg',
+            height: 185,
+            width: 120,
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.symmetric(
+                vertical: 12,
+                horizontal: 16,
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Avengers #39",
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  Text(
+                    "written by Jason Aaron",
+                    softWrap: true,
+                    style: Theme.of(context).textTheme.subtitle1,
+                  ),
+                  Text(
+                    "ENTER THE PHOENIX PROLOGUE! In the harsh, primordial world of One Million B.C.E., early humans who are different are left in the Burnt Place to die.",
+                    softWrap: true,
+                    overflow: TextOverflow.fade,
+                    maxLines: 5,
+                    style: Theme.of(context).textTheme.bodyText1,
+                  )
+                ],
+              )
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
