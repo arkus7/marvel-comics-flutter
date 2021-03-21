14926dd71d0e227b4eb07a93ca054346786a0002
Arkadiusz Żmudzin <arek@moodup.team>
2021-03-21 23:52:26 +0100

    Add method to decode ComicBook model from json

--- lib/models/comic_book.dart
+++ lib/models/comic_book.dart
@@ -1,6 +1,15 @@
+import 'package:flutter/foundation.dart';
+
 class ComicBook {
   final String title;
   final String description;
 
-  ComicBook(this.title, this.description);
+  ComicBook({@required this.title, @required this.description});
+
+  factory ComicBook.fromJson(Map<String, dynamic> json) {
+    return ComicBook(
+      title: json['title'],
+      description: json['description'],
+    );
+  }
 }
