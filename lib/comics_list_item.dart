27427bb566b43be898ebdd59a5d257c76363379f
Arkadiusz Żmudzin <arek@moodup.team>
2021-03-21 20:57:37 +0100

    Reformat widget view

--- lib/comics_list_item.dart
+++ lib/comics_list_item.dart
@@ -18,39 +18,40 @@ class ComicsListItemWidget extends StatelessWidget {
           ),
           Expanded(
             child: Container(
-                padding: EdgeInsets.symmetric(
-                  vertical: 12,
-                  horizontal: 16,
-                ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      title,
-                      style: Theme.of(context).textTheme.headline6,
+              padding: EdgeInsets.symmetric(
+                vertical: 12,
+                horizontal: 16,
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: 8,
+                      bottom: 16,
                     ),
-                    Padding(
-                      padding: EdgeInsets.only(
-                        top: 8,
-                        bottom: 16,
-                      ),
-                      child: Text(
-                        "written by Jason Aaron",
-                        softWrap: true,
-                        style: Theme.of(context).textTheme.caption,
-                      ),
-                    ),
-                    Text(
-                      description,
+                    child: Text(
+                      "written by Jason Aaron",
                       softWrap: true,
-                      overflow: TextOverflow.fade,
-                      maxLines: 5,
-                      style: Theme.of(context).textTheme.bodyText1,
-                    )
-                  ],
-                )),
+                      style: Theme.of(context).textTheme.caption,
+                    ),
+                  ),
+                  Text(
+                    description,
+                    softWrap: true,
+                    overflow: TextOverflow.fade,
+                    maxLines: 5,
+                    style: Theme.of(context).textTheme.bodyText1,
+                  )
+                ],
+              )
+            ),
           )
         ],
       ),
