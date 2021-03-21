ba3007c1e9fe48ff323eef711df00a0ae0a4aac8
Arkadiusz Żmudzin <arek@moodup.team>
2021-03-21 16:51:06 +0100

    Add Home and Search widgets

---
+++ lib/home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomeWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new _HomeWidgetState();
+}
+
+class _HomeWidgetState extends State<HomeWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("HomeWidget");
+  }
+}
---
+++ lib/search.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SearchWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new _SearchWidgetState();
+}
+
+class _SearchWidgetState extends State<SearchWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("SearchWidget");
+  }
+}
